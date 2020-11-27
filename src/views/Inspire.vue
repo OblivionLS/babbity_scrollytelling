e5179145452d1c077175b4af78767b6adde99fe0
OblivionLS <55786167+OblivionLS@users.noreply.github.com>
2020-11-27 10:16:54 +0100

    reordering

--- src/views/Inspire.vue
+++ src/views/Inspire.vue
@@ -1,20 +1,26 @@
 <template>
   <h1>Inspiration</h1>
   <div id="background"></div>
+  <div class="window">
+  <iframe width="1600" height="900" src="https://miro.com/app/embed/o9J_ld1J6sg=/?pres=1&frameId=3074457352043663518" frameBorder="10" scrolling="no" allowFullScreen></iframe>
+  </div>
+  <h2>in case the miro board doesn't work for you</h2>
   <img src="../assets/moodboard1.png" alt="moodboard">
   <img src="../assets/moodboard2.png" alt="moodboard">
   <img src="../assets/moodboard3.png" alt="moodboard">
   <img src="../assets/moodboard4.png" alt="moodboard">
   <img src="../assets/moodboard5.png" alt="moodboard">
-  <div class="window">
-  <iframe width="1600" height="900" src="https://miro.com/app/embed/o9J_ld1J6sg=/?pres=1&frameId=3074457352043663518" frameBorder="10" scrolling="no" allowFullScreen></iframe>
-  </div>
 </template>
 
 <style scoped>
 h1{
     top:0px;
     position:fixed;
+}
+h2{
+  margin-top: 10%;
+  position:relative;
+  color: var(--text);
 }
 
 img{
