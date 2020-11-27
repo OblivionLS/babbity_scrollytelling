bdbcc3ae76dd30f1bd10ad614e4c5f07c408435a
DMaag-14 <dominik.maag14@gmail.com>
2020-11-27 09:46:00 +0100

    Update Inspire.vue

--- src/views/Inspire.vue
+++ src/views/Inspire.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>Inspiration</h1>
   <div id="background"></div>
-  <img src="../assets/Moodboard1.png">
-  <img src="../assets/Moodboard2.png">
-  <img src="../assets/Moodboard3.png">
-  <img src="../assets/Moodboard4.png">
-  <img src="../assets/Moodboard5.png">
+  <img src="../assets/moodboard1.png">
+  <img src="../assets/moodboard2.png">
+  <img src="../assets/moodboard3.png">
+  <img src="../assets/moodboard4.png">
+  <img src="../assets/moodboard5.png">
 </template>
 
 <style scoped>
