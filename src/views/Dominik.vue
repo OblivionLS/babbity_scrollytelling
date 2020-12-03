bdb4f9d4a0e643bf0d2276ba1bc5667e1b02fbe7
DMaag-14 <dominik.maag14@gmail.com>
2020-12-03 10:21:37 +0100

    scrollButton

--- src/views/Dominik.vue
+++ src/views/Dominik.vue
@@ -1,8 +1,19 @@
 <template>
-<p>Dominiks page</p>
 
-<img src="../assets/babbity.png" class="babbity">
-<img src="../assets/koenig.png" class="koenig">
+<div class="scene" id="scene1">
+    <img src="../assets/koenig.png" class="koenig">
+    <button v-on:click="this.scrollDown">Down</button>
+</div>
+
+<div class="scene" id="scene2">
+    <img src="../assets/babbity.png" class="babbity">
+    <button v-on:click="scrollDown">Down</button>
+</div>
+
+<div class="scene" id="scene3">
+    <button v-on:click="scrollDown">Down</button>
+</div>
+
 </template>
 
 <script>
@@ -17,18 +28,48 @@ let scrollAt;
 scrollAt = 0;
 console.log(scrollAt);
 
+var lastScene = 0;
+var activeScene = 1;
+var nextScene = 2;
+
 export default {
-    mounted: function(){
-        gsap.to(".vorhang1", {
-        scrollTrigger: {
-            start: "top top",
-            end: "+=500",
-            pin: true,
-            trigger: ".vorhang1",
-            scrub: 1,
+
+    methods: {
+        scrollUP: function(){
+            console.log("gooo ue!!!")
+            console.log(lastScene);
         },
-        xPercent: 100,
-        });
+        scrollDown: function(){
+            console.log("Es Got abe");
+            lastScene = activeScene;
+            var elem = document.getElementById("scene" + nextScene);
+            elem.scrollIntoView({
+                behavior: "smooth"
+            });
+            activeScene = nextScene;
+            nextScene = nextScene + 1;
+        }
     }
+
 }
+
 </script>
+
+<style>
+.scene{
+    display: block;
+}
+
+#scene1{
+    opacity: 100%;
+}
+
+#scene2{
+    opacity: 100%;
+}
+
+#scene3{
+    opacity: 0%;
+}
+
+</style>
