3a1529d7fead3ee5247033c2b330704207ef66a3
SirDragon001 <41339504+SirDragon001@users.noreply.github.com>
2020-12-04 11:31:40 +0100

    clean

--- src/views/Marco.vue
+++ src/views/Marco.vue
@@ -1,10 +1,7 @@
-<template>
-    <img src="../assets/Vorhang1.png" alt="Vorhang" class="vorhang vorhang1" />
-    <img src="../assets/Vorhang1.png" alt="Vorhang" class="vorhang vorhang2" />
-    
+<template>    
     <div class="rope elements">
         <img src="../assets/scene_all/rope.png" alt="Rope" class="rope rope1" />
-        <div class="overla<" style="z-index: 100"></div>
+        <div class="overla<" style="z-index: -6"></div>
     </div>
 
 <figure>
@@ -12,81 +9,75 @@
         Audio 000_Test
     </figcaption>
     <audio id="sound" controls="controls">
-        
-        <source src="../assets/audio/002_Rabbity_Stump.mp3" type="audio/mp3" />
-    /**
-    This did not work
-    <source src="../assets/audio/000_Test.mp3" type="audio/mp3" />
-    */
+        <source src="../assets/audio/100_full_audio.mp3" type="audio/mp3" />
     <code>audio</code>
-    element.
     </audio>    
 </figure>
 
 <p>Marcos page</p>
+
+
+<h2>Tween timeScale</h2>
+<button id="play">Play</button>
+<button id="pause">Pause</button>
+<div id="container">
+  <div id="creature"></div>
+</div>
+
 </template>
 
 <script>
+
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TweenLite } from "gsap/TweenLite";
 
-gsap.registerPlugin(ScrollTrigger);
+var rotateCD,
+    pauseTween,
+    playBtn  = document.getElementById("play"),
+    pauseBtn = document.getElementById("pause"),
+    audio    = document.createElement("audio");
 
-let screenWidth = window.innerWidth;
-console.log(screenWidth);
-let scrollAt;
-scrollAt = 0;
-console.log(scrollAt);
+audio.loop = true;
+audio.src  = "https://upload.wikimedia.org/wikipedia/en/d/d8/You_Spin_Me_Round_by_Dead_or_Alive.ogg";
 
-export default {
-  mounted: function () {
-    //=========================================================
-    //Vorhang Animation
-    //=========================================================
-    gsap.to(".vorhang1", {
-      scrollTrigger: {
-        start: "top top",
-        end: "+=500",
-        pin: true,
-        trigger: ".vorhang1",
-        scrub: true,
-      },
-      xPercent: 100,
-    });
+rotateCD = new TweenMax("#creature", 0.3, {
+  rotation: 360, 
+  ease: Linear.easeNone,
+  repeat: -1,
+  paused: true
+})
+.timeScale(0);
 
-    gsap.to(".vorhang2", {
-      scrollTrigger: {
-        start: "top top",
-        end: "+=500",
-        pin: true,
-        trigger: ".vorhang2",
-        scrub: true,
-      },
-      xPercent: -100,
-    });
-    scrollAt += 500;
-
+play.onclick = function() {
+  rotateCD.play();
+  TweenLite.to(rotateCD, 3, { timeScale: 1 });
     
-    gsap.to(".rope1", {
-      scrollTrigger: {
-        start: scrollAt,
-        end: "+=500",
-        trigger: ".rope1",
-        scrub: true,
-        markers: true,
-      },
-      opacity: 20,
-      yPercent: -190,
-      z: 10,
-    });
- },
+  pauseTween && pauseTween.kill();
+  if (audio.paused) TweenLite.set(audio, { volume: 0, playbackRate: 0.5 });  
+  TweenLite.to(audio, 3, { volume: 1, playbackRate: 1 });
+  audio.play();
 };
+
+pause.onclick = function() {
+  TweenLite.to(rotateCD, 3, { timeScale: 0, onComplete: function() { this.pause(); }});
+  
+  pauseTween && pauseTween.kill();
+  pauseTween = TweenLite.to(audio, 3, { 
+    volume: 0, 
+    playbackRate: 0.5, 
+    onComplete: audio.pause, 
+    callbackScope: audio 
+  });
+};
+
+
+
 </script>
 
 <style scoped>
 
 
-figure {
+.figure {
     margin: 0;
 }
 
@@ -99,17 +90,8 @@ figure {
   position: absolute;
 }
 
-.elements {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  position: fixed;
-  left: 0px;
-  bottom: 0px;
-}
-
 .rope {
-  position: absolute;
+  position: center;
   width: 100%;
   overflow: hidden;
   left: 0px;
@@ -117,7 +99,39 @@ figure {
 
 .rope1 {
   height: 20%;
-  z-index: 6;
+  z-index: -6;
   bottom: -20%;
+}
+
+body {
+  background-color: #333;
+  color: white;
+}
+
+#container {
+  background-color: black;
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  position: relative;
+}
+
+#creature {
+  background: url(https://www.greensock.com/_img/codepen/bezierCreature/creature_red.png);
+  width: 27px;
+  height: 29px;
+  left: 87px;
+  top: 87px;
+  position: absolute;
+}
+
+button {
+  padding: 10px;
+  margin: 0 10px 10px 0;
+}
+
+h2 {
+  font-family: sans-serif;
+  font-weight: normal;
 }
 </style>
