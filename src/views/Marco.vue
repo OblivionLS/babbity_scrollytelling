51798c0ddb8b8bed3c66e11c6000d9137ee19859
SirDragon001 <41339504+SirDragon001@users.noreply.github.com>
2020-12-04 10:07:48 +0100

    rope

--- src/views/Marco.vue
+++ src/views/Marco.vue
@@ -1,4 +1,12 @@
 <template>
+    <img src="../assets/Vorhang1.png" alt="Vorhang" class="vorhang vorhang1" />
+    <img src="../assets/Vorhang1.png" alt="Vorhang" class="vorhang vorhang2" />
+    
+    <div class="rope elements">
+        <img src="../assets/scene_all/rope.png" alt="Rope" class="rope rope1" />
+        <div class="overla<" style="z-index: 100"></div>
+    </div>
+
 <figure>
     <figcaption>
         Audio 000_Test
@@ -12,17 +20,104 @@
     */
     <code>audio</code>
     element.
-    </audio>
+    </audio>    
 </figure>
 
 <p>Marcos page</p>
 </template>
 
 <script>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+let screenWidth = window.innerWidth;
+console.log(screenWidth);
+let scrollAt;
+scrollAt = 0;
+console.log(scrollAt);
+
+export default {
+  mounted: function () {
+    //=========================================================
+    //Vorhang Animation
+    //=========================================================
+    gsap.to(".vorhang1", {
+      scrollTrigger: {
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        trigger: ".vorhang1",
+        scrub: true,
+      },
+      xPercent: 100,
+    });
+
+    gsap.to(".vorhang2", {
+      scrollTrigger: {
+        start: "top top",
+        end: "+=500",
+        pin: true,
+        trigger: ".vorhang2",
+        scrub: true,
+      },
+      xPercent: -100,
+    });
+    scrollAt += 500;
+
+    
+    gsap.to(".rope1", {
+      scrollTrigger: {
+        start: scrollAt,
+        end: "+=500",
+        trigger: ".rope1",
+        scrub: true,
+        markers: true,
+      },
+      opacity: 20,
+      yPercent: -190,
+      z: 10,
+    });
+ },
+};
 </script>
 
 <style scoped>
+
+
 figure {
     margin: 0;
+}
+
+.vorhang {
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  z-index: 50;
+  position: absolute;
+}
+
+.elements {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: fixed;
+  left: 0px;
+  bottom: 0px;
+}
+
+.rope {
+  position: absolute;
+  width: 100%;
+  overflow: hidden;
+  left: 0px;
+}
+
+.rope1 {
+  height: 20%;
+  z-index: 6;
+  bottom: -20%;
 }
 </style>
---
+++ src/views/Marco_Test.vue
@@ -0,0 +1,70 @@
+<template>
+   <h1 class="header-section">Scroll to construct a shoe</h1>
+
+<section class="shoe">
+  <img src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84da29b46cb1403688b139_outsole.png" class="outsole piece" alt="outsole">
+  <img src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84d9ecb32e46bc5f1f8a8a_component.png?" class="heel piece" alt="heel">
+  <img src="https://uploads-ssl.webflow.com/5d3ee7c0c89f6a2d122c0695/5d84da293b7f75e5a42bcacd_midsole.png?" class="midsole piece" alt="midsole">
+</section>
+
+<h1 class="header-section">Cool, right?</h1>
+
+
+
+
+<header>
+   <a href="https://greensock.com/scrolltrigger">
+     <img class="greensock-icon" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/scroll-trigger-logo-light.svg" width="200" height="64" />
+  </a> 
+</header>
+<p>Marcos page</p>
+</template>
+
+<script>
+import { gsap } from "gsap";
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger: ".shoe",
+    start: "center center",
+    end: "bottom top",
+    scrub: true,
+    pin: true
+  }
+})
+  .from(".midsole",  { y: innerHeight * 1.5 })
+  .from(".outsole", { y: innerHeight * 1.5 });
+</script>
+
+<style scoped>
+body { 
+  background-color: #a62e91;
+  color: white;
+}
+
+
+.shoe {
+  position: relative;
+  padding: 20px;
+}
+.piece:not(.outsole) {
+  position: absolute;
+  bottom: 7vw;
+}
+.midsole {
+  width: 89%;
+  left: 5%;
+}
+.heel {
+  width: 41.8013857%;
+  left: 6%;
+}
+.outsole {
+  width: 100%;
+  position: relative;
+}
+
+h1, h2, p, li {
+  max-width: none;
+}
+</style>
